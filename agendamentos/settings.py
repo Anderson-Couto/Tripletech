88414bde81e7b10127882c304e75c3c677c9d5ce
Anderson Couto Cardoso <andersoncouto57@gmail.com>
2020-05-11 02:50:46 -0300

    Solucionando erro no database

--- agendamentos/settings.py
+++ agendamentos/settings.py
@@ -76,19 +76,22 @@ WSGI_APPLICATION = 'agendamentos.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    #    'default': {
-    #        'ENGINE': 'django.db.backends.sqlite3',
-    #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #    }
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tripletech',
-        'USER': 'root',
-        'PASSWORD': '321abd4567',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 }
+"""
+'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'tripletech',
+    'USER': 'root',
+    'PASSWORD': '321abd4567',
+    'HOST': 'localhost',
+    'PORT': '3306',
+    
+}
+"""
 
 
 # Password validation
